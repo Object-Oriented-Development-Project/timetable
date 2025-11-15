ffbe86ab84d8fcbfe87222f8019382fca2d98ace
CalumPrendergast06 <24425478@studentmail.ul.ie>
2025-11-15 23:24:36 +0000

    Final javadoc changes for Labroom 24425478

--- timetablesysytem/src/main/java/one/group/models/rooms/Labroom.java
+++ timetablesysytem/src/main/java/one/group/models/rooms/Labroom.java
@@ -1,7 +1,16 @@
 package one.group.models.rooms;
 
+/**
+*Represents a Labratory room
+*/
 public class Labroom extends Room {
 
+    /**
+    *Creates a new labroom with the specified identifier and capacity.
+    *
+    *@param ID          the unique identifier for the lab room
+    *@param capacity the maximum number of people the lab room can hold
+    */
     public Labroom(String id, int capacity){
         super(id, capacity);
     }
