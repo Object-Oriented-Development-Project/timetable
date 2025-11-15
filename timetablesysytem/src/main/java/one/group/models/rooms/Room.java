c4d9dcf1d692dfc908eca903885bb06124e2c672
CalumPrendergast06 <24425478@studentmail.ul.ie>
2025-11-15 17:26:58 +0000

    Update Room.java

--- timetablesysytem/src/main/java/one/group/models/rooms/Room.java
+++ timetablesysytem/src/main/java/one/group/models/rooms/Room.java
@@ -9,9 +9,37 @@ public abstract class Room {
     public Room(String id, int capacity){
         this.id = id;
         this.capacity = capacity;
+        this.bookings = new ArrayList<>();
     }
-
+    public int getId () {
+        return id;
+    }
+    
     public int getCapacity() {
         return capacity;
+    }
+    public List <TimetableSlot> getBookings() {
+        return bookings;
+    }
+    public boolean isAvailable (TimetableSlot slot) {
+        for (TimetableSlot existingslot : bookings) {
+            if (existingSlot.overlaps(slot)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean addBooking(TimetableSlot slot) {
+        if (isAvailable(slot)) {
+            bookings.add(slot);
+            return true;
+        }
+        return false
+            }
+    public abstract RoomType getRoomType();
+
+    @Override
+    public String toString() {
+        return id + "(" + getRoomType() + ", capacity=" + capacity + ")";
     }
 }
