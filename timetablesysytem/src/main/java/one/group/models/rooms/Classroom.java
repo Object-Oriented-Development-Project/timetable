ed8b9e1ded1db14477aa93ae129da17f38cd304c
CalumPrendergast06 <24425478@studentmail.ul.ie>
2025-11-15 22:46:49 +0000

    Javadocs for classroom 24425478

--- timetablesysytem/src/main/java/one/group/models/rooms/Classroom.java
+++ timetablesysytem/src/main/java/one/group/models/rooms/Classroom.java
@@ -1,8 +1,17 @@
 package one.group.models.rooms;
 
+/**
+*Represents a standard classroom within the timetable system.
+*A classroom is a concrete type of room with an id and capacity.
+*/ 
 public class Classroom extends Room {
 
-    /**Creates Classroom object using String id and int capacity. */
+    /**
+    *Creates a classroom object using a unique room ID and capacity.
+    *
+    * @param id          the unique identifier for the classroom
+    * @param capacity maximum number of people the classroom can hold
+    */
     public Classroom(String id, int capacity){
         super(id, capacity);
     }
