44b27f8f6378cfb1acc04bca689bc1fa8671d103
Jake Sheehy <murphyeddie166@gmail.com>
2025-11-17 17:02:48 +0000

    added interim design docs to the repo, modified gitignore to allow that specific .zip file

--- timetablesysytem/src/main/java/one/group/models/programmes/ProgramStructure.java
+++ timetablesysytem/src/main/java/one/group/models/programmes/ProgramStructure.java
@@ -7,7 +7,7 @@ import one.group.models.people.Student;
 import one.group.models.user_timetable.Timetable;
 
 /** Class representing a course. */
-public class ProgrammeStructure {
+public class ProgramStructure {
     /** The id of the course. */
     private String courseID;
     /** The name of the course. */
