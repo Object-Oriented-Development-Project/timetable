08f10cf5abdbd422b0f852dd2188580a8d57aed4
Jake Sheehy <murphyeddie166@gmail.com>
2025-11-22 20:28:06 +0000

    Javadocs for TablesRepo.

--- timetablesysytem/src/main/java/one/group/models/repositories/TablesRepo.java
+++ timetablesysytem/src/main/java/one/group/models/repositories/TablesRepo.java
@@ -8,16 +8,24 @@ import one.group.models.user_timetable.CSVReader;
  * Includes methods to store each csv file as an ArrayList on boot, get methods only return copies.
  */
 public class TablesRepo {
-
+    /** ArrayList for the table of admins. */
     private static ArrayList<String[]> adminTable;
+    /** ArrayList for the table of courses. */
     private static ArrayList<String[]> coursesTable;
+    /** ArrayList for the table of faculties. */
     private static ArrayList<String[]> facultiesTable;
+    /** ArrayList for the table of lecturers. */
     private static ArrayList<String[]> lecturersTable;
+    /** ArrayList for the table of modules. */
     private static ArrayList<String[]> modulesTable;
+    /** ArrayList for the table of rooms. */
     private static ArrayList<String[]> roomsTable;
+    /** ArrayList for the table of students. */
     private static ArrayList<String[]> studentsTable;
+    /** ArrayList for the table of terms. */
     private static ArrayList<String[]> termsTable;
 
+    /** Static block to initialise all tables on boot from the CSV files specified by the given paths.  */
     static {
         try {
             adminTable = CSVReader.readToArrayList(CSVTable.ADMINS_TABLE.filePath);
@@ -33,34 +41,66 @@ public class TablesRepo {
         }
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getAdminTable(){
         return new ArrayList<>(adminTable);
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getCoursesTable(){
         return new ArrayList<>(coursesTable);
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getFacultiesTable(){
         return new ArrayList<>(facultiesTable);
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getLecturersTable(){
         return new ArrayList<>(lecturersTable);
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getModulesTable(){
         return new ArrayList<>(modulesTable);
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getRoomsTable(){
         return new ArrayList<>(roomsTable);
     }
 
+    /**
+     * Method to return the admins table
+     * @return adminTable the table of admins
+     */
     public static ArrayList<String[]> getStudentsTable(){
         return new ArrayList<>(studentsTable);
     }
 
+    /**
+     * Method to return the terms table
+     * @return termsTable the table of admins
+     */
     public static ArrayList<String[]> getTermsTable(){
         return new ArrayList<>(termsTable);
     }
