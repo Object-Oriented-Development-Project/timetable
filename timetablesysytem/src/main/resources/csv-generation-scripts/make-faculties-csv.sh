b13b84e025be392af8044520cd45ec2e89af59dd
Jake Sheehy <murphyeddie166@gmail.com>
2025-11-27 15:11:20 +0000

    Changes to CSV files data.

--- timetablesysytem/src/main/resources/csv-generation-scripts/make-faculties-csv.sh
+++ timetablesysytem/src/main/resources/csv-generation-scripts/make-faculties-csv.sh
@@ -9,7 +9,7 @@ echo "FACULTY ID,NAME,BUILDING" > "$file"
 # Example data as arrays
 id=("CSIS" "ENG" "SCI")
 name=("COMPUTER SCIENCE AND INFORMATION SYSTEMS" "ENGINEERING" "SCIENCE")
-building=("CSG" "EG" "KBG")
+building=("CS" "E" "KBS")
 
 # Loop through data and append to CSV
 for i in "${!id[@]}"; do
